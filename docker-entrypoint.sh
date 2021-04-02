7ceebe3631af32d58baf923be014ca2c06f330b3
Mark Greenway <MarkGreenwayIT@LIGMarine.com>
2021-04-02 17:43:13 -0400

    It can't be the spaces

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@
 
 while [ : ]
 do
-     REQUEST_URL = "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID"
+     REQUEST_URL="https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID"
      now="$(date +"%r")"
      IP=$(curl --ipv4 -s http://icanhazip.com/)
      printf "\n$now\t-\t$IP\t-\t$REQUEST_URL\n"
