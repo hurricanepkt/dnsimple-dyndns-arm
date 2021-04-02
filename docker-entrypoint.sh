36c44286ab85b5c51fcc594405424c2269274d8a
Mark Greenway <MarkGreenwayIT@LIGMarine.com>
2021-04-02 00:34:45 -0400

    echos everywhere

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 [ "$1" = "update" ] || exec "$@" || exit $?
 
+echo "Hello World"
 
 IP=$(curl --ipv4 -s http://icanhazip.com/)
 echo "IP"
@@ -22,3 +23,5 @@ curl -H "Authorization: Bearer $TOKEN" \
      -X "PATCH" \
      -i "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID" \
      -d "{\"content\":\"$IP\"}"
+
+echo "Done"
