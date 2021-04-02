24966fe8caa26a2f105c6ff158c2bcfc4fa13b41
Mark Greenway <MarkGreenwayIT@LIGMarine.com>
2021-04-02 17:17:53 -0400

    While Loop again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,15 +3,18 @@
 [ "$1" = "update" ] || exec "$@" || exit $?
 
 
+while [ : ]
+do
+     IP=$(curl --ipv4 -s http://icanhazip.com/)
 
-IP=$(curl --ipv4 -s http://icanhazip.com/)
 
+     curl -H "Authorization: Bearer $TOKEN" \
+          -H "Content-Type: application/json" \
+          -H "Accept: application/json" \
+          -X "PATCH" \
+          -i "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID" \
+          -d "{\"content\":\"$IP\"}" \ 
+          --silent
 
-curl -H "Authorization: Bearer $TOKEN" \
-     -H "Content-Type: application/json" \
-     -H "Accept: application/json" \
-     -X "PATCH" \
-     -i "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID" \
-     -d "{\"content\":\"$IP\"}" \ 
-     --silent
-
+     sleep $SLEEP_DELAY
+done
