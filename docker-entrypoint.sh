e0ebcc3543b9a1b33b37600ccd02568a7735e4eb
Mark Greenway <MarkGreenwayIT@LIGMarine.com>
2021-04-02 00:42:25 -0400

    One liners

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,17 +5,15 @@
 echo "Hello World"
 
 IP=$(curl --ipv4 -s http://icanhazip.com/)
-echo "IP"
-echo $IP
 
-echo "ACCOUNT_ID"
-echo $ACCOUNT_ID
 
-echo "ZONE_ID"
-echo $ZONE_ID
-
-echo "RECORD_ID"
-echo $RECORD_ID
+echo "IP $IP"
+echo "TOKEN $TOKEN"
+echo "ACCOUNT_ID $ACCOUNT_ID"
+echo "ZONE_ID $ZONE_ID"
+echo "RECORD_ID $RECORD_ID"
+echo "URL  https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID "
+echo "Sending..."
 
 curl -H "Authorization: Bearer $TOKEN" \
      -H "Content-Type: application/json" \
