04f4ab3a205120782e9999eb51c443da0041a881
Mark Greenway <MarkGreenwayIT@LIGMarine.com>
2021-04-02 00:52:43 -0400

    silent

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,24 +2,16 @@
 
 [ "$1" = "update" ] || exec "$@" || exit $?
 
-echo "Hello World"
+
 
 IP=$(curl --ipv4 -s http://icanhazip.com/)
 
-
-echo "IP $IP"
-echo "TOKEN $TOKEN"
-echo "ACCOUNT_ID $ACCOUNT_ID"
-echo "ZONE_ID $ZONE_ID"
-echo "RECORD_ID $RECORD_ID"
-echo "URL  https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID "
-echo "Sending..."
 
 curl -H "Authorization: Bearer $TOKEN" \
      -H "Content-Type: application/json" \
      -H "Accept: application/json" \
      -X "PATCH" \
      -i "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID" \
-     -d "{\"content\":\"$IP\"}"
+     -d "{\"content\":\"$IP\"}" \ 
+     --silent
 
-echo "Done"
