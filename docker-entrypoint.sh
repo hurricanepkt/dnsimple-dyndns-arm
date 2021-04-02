bae36a485a9762e86a32eccbdb8070cafa9b6aa8
Mark Greenway <MarkGreenwayIT@LIGMarine.com>
2021-04-02 17:39:36 -0400

    REQUEST_URL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,19 @@
 
 [ "$1" = "update" ] || exec "$@" || exit $?
 
-URL = "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID"
+
 while [ : ]
 do
+     REQUEST_URL = "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records/$RECORD_ID"
      now="$(date +"%r")"
      IP=$(curl --ipv4 -s http://icanhazip.com/)
-     printf "\n$now\t-\t$IP\t-\t$URL\n"
+     printf "\n$now\t-\t$IP\t-\t$REQUEST_URL\n"
 
      curl -H "Authorization: Bearer $TOKEN" \
           -H "Content-Type: application/json" \
           -H "Accept: application/json" \
           -X "PATCH" \
-          $URL \
+          $REQUEST_URL \
           -d "{\"content\":\"$IP\"}" \ 
 
      sleep $SLEEP_DELAY
